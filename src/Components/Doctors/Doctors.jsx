26b18f30bf407daa277c0315d9995d6b1afb7ac0
sohagroy7223 <freelancersohagroy@gmail.com>
2026-03-05 23:42:10 +0600

    addad a use state that 1 st show some items and add a btn event handelar click to all items show

--- src/Components/Doctors/Doctors.jsx
+++ src/Components/Doctors/Doctors.jsx
@@ -1,19 +1,25 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Doctor from "../Doctor/Doctor";
 
 const Doctors = ({ doctorsPromise }) => {
   const doctors = use(doctorsPromise);
-  console.log(doctors);
+  const [showAll, setShowAll] = useState(false);
+
+  const DisplayDoctors = showAll ? doctors : doctors.slice(0, 6);
+
   return (
     <div>
       <h3>all doctors is here {doctors.length}</h3>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-2">
-        {doctors.map((doctor) => (
+        {DisplayDoctors.map((doctor) => (
           <Doctor key={doctor.id} doctor={doctor}></Doctor>
         ))}
       </div>
       <span className="w-full ">
-        <button className="btn mt-3.5 bg-green-600 mx-auto flex justify-center ">
+        <button
+          onClick={() => setShowAll(!showAll)}
+          className="btn mt-3.5 bg-green-600 mx-auto flex justify-center "
+        >
           View All Doctors
         </button>
       </span>
