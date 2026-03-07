b8046f90d1af91735eb11f2fb7f69be4ab144167
sohagroy7223 <freelancersohagroy@gmail.com>
2026-03-07 22:31:12 +0600

    addad counted hover animation in this page

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import success from "../../assets/success-doctor.png";
 import success1 from "../../assets/success-patients.png";
 import success3 from "../../assets/success-review.png";
 import success4 from "../../assets/success-staffs.png";
+import CountUp from "react-countup";
 
 const Services = () => {
+  const [hover, setHover] = useState(false);
   return (
-    <div className=" rounded-2xl mt-20 p-5">
+    <div className=" rounded-2xl mt-20 p-5" onMouseEnter={() => setHover(true)}>
       <h2 className="text-3xl font-bold text-center">
         We Provide Best Medical Services
       </h2>
@@ -17,22 +19,30 @@ const Services = () => {
       <div className=" grid grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
         <div className="w-full mx-auto bg-white p-10 flex flex-col justify-center rounded-2xl ">
           <img className="w-11 " src={success} alt="" />
-          <h3 className="text-5xl mt-2 mb-1 font-bold">199+</h3>
+          <h3 className="text-5xl mt-2 mb-1 font-bold">
+            {hover && <CountUp end={199} duration={10}></CountUp>}+
+          </h3>
           <p>Total Doctors</p>
         </div>
         <div className="w-full mx-auto bg-white p-10 flex flex-col justify-center rounded-2xl ">
           <img className="w-11 " src={success1} alt="" />
-          <h3 className="text-5xl mt-2 mb-1 font-bold">467+</h3>
+          <h3 className="text-5xl mt-2 mb-1 font-bold">
+            {hover && <CountUp end={467} duration={10}></CountUp>}+
+          </h3>
           <p>Total Reviews</p>
         </div>
         <div className="w-full mx-auto bg-white p-10 flex flex-col justify-center rounded-2xl ">
           <img className="w-11 " src={success3} alt="" />
-          <h3 className="text-5xl mt-2 mb-1 font-bold">1900+</h3>
+          <h3 className="text-5xl mt-2 mb-1 font-bold">
+            {hover && <CountUp end={1900} duration={10}></CountUp>}+
+          </h3>
           <p>Patients</p>
         </div>
         <div className="w-full mx-auto bg-white p-10 flex flex-col justify-center rounded-2xl ">
           <img className="w-11  " src={success4} alt="" />
-          <h3 className="text-5xl mt-2 mb-1 font-bold">300+</h3>
+          <h3 className="text-5xl mt-2 mb-1 font-bold">
+            {hover && <CountUp end={300} duration={10}></CountUp>}+
+          </h3>
           <p>Total Stuffs</p>
         </div>
       </div>
